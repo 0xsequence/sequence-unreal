44863b0d36f9f7ed0efac3c97fcd4b3ebd7517f1
Jan Dohring <jan.dohring@zemind.ca>
2023-04-24 10:14:13 -0400

    removed submodule

---
+++ Plugins/SequencePlugin/Source/SequencePlugin/Private/Bitcoin-Cryptography-Library/cpp/CountOps.hpp
@@ -0,0 +1,21 @@
+/* 
+ * Bitcoin cryptography library
+ * Copyright (c) Project Nayuki
+ * 
+ * https://www.nayuki.io/page/bitcoin-cryptography-library
+ * https://github.com/nayuki/Bitcoin-Cryptography-Library
+ */
+
+#pragma once
+
+
+void countOps(long n);
+	
+enum {
+	functionOps = 1,
+	arithmeticOps = 1,
+	loopBodyOps = 3,  // Increment, compare, jump
+	uint256CopyOps = 17,
+	fieldintCopyOps = 18,  // 1 + uint256CopyOps
+	curvepointCopyOps = 55,  // 1 + 3 * fieldintCopyOps
+};
