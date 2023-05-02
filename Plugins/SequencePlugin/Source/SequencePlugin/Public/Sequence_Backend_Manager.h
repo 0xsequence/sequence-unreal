91e2a333d72752e8da1d2e9764088bf91eab3848
CalvinKork <calvin.kork@zemind.ca>
2023-05-02 13:56:18 -0500

    Updates to the backend manager and resolving bugs with the hex utility

--- Plugins/SequencePlugin/Source/SequencePlugin/Public/Sequence_Backend_Manager.h
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Sequence_Backend_Manager.h
@@ -18,6 +18,8 @@ public:
 	UFUNCTION(BlueprintCallable)
 		FString Setup();
 
+	UFUNCTION(BlueprintCallable)
+		FString Testing_Address_Generation();
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
