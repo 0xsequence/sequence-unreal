dec74f8ba9b6b472d2c21f4ca378995f1d7f18eb
Jan Dohring <jan.dohring@zemind.ca>
2023-05-10 12:23:20 -0400

    added detail to test transaction

--- Plugins/SequencePlugin/Source/SequencePlugin/Public/Tests/TestTransaction.cpp
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Tests/TestTransaction.cpp
@@ -20,27 +20,52 @@ bool TestTransaction::RunTest(const FString& Parameters)
 	auto VALUE = "0x1"; // 0xDE0B6B3A7640000;
 	auto PRIVATE_KEY = "0xabc0000000000000000000000000000000000000000000000000000000000001";
 
+	// This is the first private key in the hardhat config
+	// should correspond to address 0xc683a014955b75F5ECF991d4502427c8fa1Aa249
+	// We can see in the logging that we indeed generate the same address
+
+	// currently following the standard set by https://eips.ethereum.org/EIPS/eip-155
+	auto USING_EIP155 = true;
 	
 	// Make the test pass by returning tru e, or fail by returning false.
 
 	// signing data
-	// currently following 
-	auto item = Itemize(new RLPItem[]
-	{
-		Itemize(HexStringtoBinary(NONCE)), // Nonce
-		Itemize(HexStringtoBinary(GASPRICE)), // GasPrice
-		Itemize(HexStringtoBinary(GASLIMIT)), // GasLimit
-		Itemize(HexStringtoBinary(TO)), // To
-		Itemize(HexStringtoBinary(VALUE)), // Value
-		Itemize(HexStringtoBinary("0x")), // Data
-		Itemize(HexStringtoBinary("0x1")), // V
-		Itemize(HexStringtoBinary("0x")), // R 
-		Itemize(HexStringtoBinary("0x")), // S
-	}, 9);
-
-	auto EncodedSigningData = RLP::Encode(item);
 	
-	//signing hash
+	RLPItem item;
+
+	if(USING_EIP155)
+	{
+		item = Itemize(new RLPItem[]
+	    {
+			Itemize(HexStringtoBinary(NONCE)), // Nonce
+			Itemize(HexStringtoBinary(GASPRICE)), // GasPrice
+			Itemize(HexStringtoBinary(GASLIMIT)), // GasLimit
+			Itemize(HexStringtoBinary(TO)), // To
+			Itemize(HexStringtoBinary(VALUE)), // Value
+			Itemize(HexStringtoBinary("0x")), // Data
+			Itemize(HexStringtoBinary("0x1")), // V
+			Itemize(HexStringtoBinary("0x")), // R 
+			Itemize(HexStringtoBinary("0x")), // S
+	    }, 9);
+	}
+	else
+	{
+		item = Itemize(new RLPItem[]
+		{
+			Itemize(HexStringtoBinary(NONCE)), // Nonce
+			Itemize(HexStringtoBinary(GASPRICE)), // GasPrice
+			Itemize(HexStringtoBinary(GASLIMIT)), // GasLimit
+			Itemize(HexStringtoBinary(TO)), // To
+			Itemize(HexStringtoBinary(VALUE)), // Value
+			Itemize(HexStringtoBinary("0x")), // Data
+		}, 6);
+	}
+
+	auto EncodedSigningData = RLP::Encode(item); // RLP Encoder creates data to be hashed
+	
+	// signing hash
+	// Verify using https://emn178.github.io/online-tools/keccak_256.html
+	// Looks correct to me - Jan
 	Hash256 SigningHash = new uint8[GHash256ByteLength];
 	Keccak256::getHash(EncodedSigningData.Data, EncodedSigningData.ByteLength, SigningHash);
 
@@ -48,19 +73,35 @@ bool TestTransaction::RunTest(const FString& Parameters)
 	PrivateKey Privkey = HexStringToHash(GPrivateKeyByteLength, PRIVATE_KEY);
 	PublicKey PubKey = GetPublicKey(Privkey);
 	Address Addr = GetAddress(PubKey);
-	uint8 MyR[GHash256ByteLength];
-	uint8 MyS[GHash256ByteLength];
-	Uint256 R, S;
-	auto msgHash = Sha256Hash(SigningHash, GHash256ByteLength);
+	uint8 MyR[GHash256ByteLength]; // Buffer for R to be transferred to
+	uint8 MyS[GHash256ByteLength]; // Buffer for S to be transferred to
+	Uint256 R, S; // These have to be Nayuki's special Uint256s
+	auto msgHash = Sha256Hash(SigningHash, GHash256ByteLength); // We must convert our hash to Nayuki's Sha256Hash 
+
+	// Usually EDSCA uses a random nonce value that has to be generated
+	// Nowadays crypto libraries get this random nonce deterministically from the private key and hash
+	// That is what signWithHmacNonce does
+	auto b = Ecdsa::signWithHmacNonce(Uint256(PRIVATE_KEY), msgHash, R, S); 
+	R.getBigEndianBytes(MyR); // transfer data
+	S.getBigEndianBytes(MyS); // transfer data
 	
-	auto b = Ecdsa::signWithHmacNonce(Uint256(PRIVATE_KEY), msgHash, R, S);
-	R.getBigEndianBytes(MyR);
-	S.getBigEndianBytes(MyS);
 	auto ChainId = 1337;
-	uint16 V = ((MyR[GHash256ByteLength - 1] + 0) % 2) + ChainId * 2 + 35; // See https://eips.ethereum.org/EIPS/eip-155#parameters
+	uint16 V;
+
+	// See https://eips.ethereum.org/EIPS/eip-155#parameters
+	if(USING_EIP155)
+	{
+		V = (MyR[GHash256ByteLength - 1] % 2) + ChainId * 2 + 35; 
+	}
+	else
+	{
+		V = (MyR[GHash256ByteLength - 1] % 2) + 27;
+	}
+
+	// Now we just need to combine our data into one RLP encoded hex string and send it
 	
 	//signing data
-	auto item2 = Itemize(new RLPItem[]
+	auto FullTransaction = Itemize(new RLPItem[]
 	{
 		Itemize(HexStringtoBinary(NONCE)), // Nonce
 		Itemize(HexStringtoBinary(GASPRICE)), // GasPrice
@@ -73,15 +114,18 @@ bool TestTransaction::RunTest(const FString& Parameters)
 		Itemize(HexStringtoBinary(Hash256ToHexString(MyS))),  // S
 	}, 9);
 
-	auto SignedTransaction = RLP::Encode(item2);
+	auto SignedTransaction = RLP::Encode(FullTransaction);
 	auto SignedTransactionString = BinaryToHexString(SignedTransaction);
 
 	auto provider = Provider("http://localhost:8545/");
 	provider.SendRawTransaction("0x" + SignedTransactionString);
 
-	auto CurvePointPubKey = CurvePoint::privateExponentToPublicPoint(Uint256(PRIVATE_KEY));
-	auto isVerified = Ecdsa::ecdsa_verify(CurvePointPubKey, msgHash, R, S);
 
+	// This will tell us if the crypto library is internally consistent, that is, it can verify that our transaction
+	//  is indeed signed using the same private key
+	// It has been looking correct so far - Jan
+	auto CurvePointPubKey = CurvePoint::privateExponentToPublicPoint(Uint256(PRIVATE_KEY)); // Convert private key to x, y point on secp256k1 curve
+	auto isVerified = Ecdsa::ecdsa_verify(CurvePointPubKey, msgHash, R, S);
 	if(isVerified)
 	{
 		UE_LOG(LogTemp, Display, TEXT("Verified!"));
@@ -91,10 +135,13 @@ bool TestTransaction::RunTest(const FString& Parameters)
 		UE_LOG(LogTemp, Display, TEXT("Not Verified!"));
 	}
 
-	// Print Info
+	// Print Info for afterwards
+	// Verify RLP encoded data using https://flightwallet.github.io/decode-eth-tx/
+	// Everything seems to match up correctly except that the R and S values generate the wrong address :( - Jan
 	{
 		UE_LOG(LogTemp, Display, TEXT("ENCODED SIGNING DATA: %s"), *BinaryToHexString(EncodedSigningData));
 		UE_LOG(LogTemp, Display, TEXT("ENCODED SIGNING HASH: %s"), *Hash256ToHexString(SigningHash));
+		UE_LOG(LogTemp, Display, TEXT("SIGNED TRANSACTION STRING: %s"), *SignedTransactionString);
 		UE_LOG(LogTemp, Display, TEXT("PUBKEY: %s"), *HashToHexString(GPublicKeyByteLength, PubKey));
 		UE_LOG(LogTemp, Display, TEXT("ADDR: %s"), *HashToHexString(GAddressByteLength, Addr));
 		UE_LOG(LogTemp, Display, TEXT("V: %s"), *IntToHexString(V));
