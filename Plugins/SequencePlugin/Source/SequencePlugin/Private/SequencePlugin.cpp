54e8d45065f4e4edd496d1df734dbe4aeae19c91
CalvinKork <calvin.kork@zemind.ca>
2023-05-15 10:25:36 -0500

    Recompiled and removed lots of dead code

--- Plugins/SequencePlugin/Source/SequencePlugin/Private/SequencePlugin.cpp
+++ Plugins/SequencePlugin/Source/SequencePlugin/Private/SequencePlugin.cpp
@@ -24,10 +24,6 @@ void FSequencePluginModule::ShutdownModule()
 
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
-
-	// Free the dll handle
-	FPlatformProcess::FreeDllHandle(ExampleLibraryHandle);
-	ExampleLibraryHandle = nullptr;
 }
 
 #undef LOCTEXT_NAMESPACE
