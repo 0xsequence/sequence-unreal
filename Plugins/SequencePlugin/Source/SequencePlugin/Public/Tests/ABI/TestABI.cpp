abeaad8babbc010e402e2fbda9a1bd354499df82
Jan Dohring <jan.dohring@zemind.ca>
2023-06-29 12:08:35 -0400

    Int Changes

--- Plugins/SequencePlugin/Source/SequencePlugin/Public/Tests/ABI/TestABI.cpp
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Tests/ABI/TestABI.cpp
@@ -1,7 +1,7 @@
 #include "ABI/ABI.h"
 #include "HexUtility.h"
 #include "Misc/AutomationTest.h"
-#include "Types/ABITypes.h"
+#include "ABI/ABITypes.h"
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(TestABI, "Public.TestABI",
                                  EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
