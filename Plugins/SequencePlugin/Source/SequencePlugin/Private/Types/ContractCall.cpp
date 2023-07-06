123c85ca844139b2e23f0aba39adb2a136cca546
Vaibhav Valecha <vaibhav.valecha@zemind.ca>
2023-07-06 11:03:37 -0400

    estimate gas for contract call

--- Plugins/SequencePlugin/Source/SequencePlugin/Private/Types/ContractCall.cpp
+++ Plugins/SequencePlugin/Source/SequencePlugin/Private/Types/ContractCall.cpp
@@ -1,5 +1,6 @@
 #include "Types/ContractCall.h"
 
+#include "HexUtility.h"
 #include "JsonBuilder.h"
 
 FString ContractCall::GetJson()
@@ -10,7 +11,7 @@ FString ContractCall::GetJson()
 	json.AddString("to", "0x" + To.ToHex());
 	if(Gas.IsSet()) json.AddString("gas", FString::FromInt(Gas.GetValue()));
 	if(GasPrice.IsSet()) json.AddString("gasPrice", FString::FromInt(GasPrice.GetValue()));
-	if(Value.IsSet()) json.AddString("value", FString::FromInt(Value.GetValue()));
+	if(Value.IsSet()) json.AddString("value", IntToHexString(Value.GetValue()));
 	if(Data.IsSet()) json.AddString("data", "0x" + Data.GetValue());
 
 	return json.ToString();
