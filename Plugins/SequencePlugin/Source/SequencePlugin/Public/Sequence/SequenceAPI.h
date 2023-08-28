2c5d64cc14ad4f1726eabb601f335d14892dd3dd
Jan Dohring <jan.dohring@zemind.ca>
2023-08-28 16:42:29 -0400

    error propagation

--- Plugins/SequencePlugin/Source/SequencePlugin/Public/Sequence/SequenceAPI.h
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Sequence/SequenceAPI.h
@@ -116,7 +116,7 @@ namespace SequenceAPI
 		void SendTransaction(FTransaction Transaction, TSuccessCallback<FHash256> OnSuccess, FFailureCallback OnFailure);
 		void SendTransactionBatch(TArray<FTransaction> Transactions, TSuccessCallback<FHash256> OnSuccess, FFailureCallback OnFailure);
 
-		void SendTransactionWithCallback(FTransaction_FE Transaction, TSuccessCallback<FString> OnSuccess, FFailureCallback OnFailure);
+		void SendTransactionWithCallback(FTransaction_FE Transaction, TSuccessCallback<FString> OnSuccess, TFunction<void (FString, SequenceError)> OnFailure);
 
 		void getFriends(FString publicAddress, TSuccessCallback<TArray<FContact_BE>> OnSuccess, FFailureCallback OnFailure);
 
