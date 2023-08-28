33bcda2b47725fbff5991f137fb0b41106be9da3
Jan Dohring <jan.dohring@zemind.ca>
2023-08-28 12:49:20 -0400

    Uncomment

--- Plugins/SequencePlugin/Source/SequencePlugin/Public/Sequence/SequenceAPI.cpp
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Sequence/SequenceAPI.cpp
@@ -430,11 +430,11 @@ void SequenceAPI::FSequenceWallet::SendTransactionBatch(TArray<FTransaction> Tra
 void SequenceAPI::FSequenceWallet::SendTransactionWithCallback(FTransaction_FE Transaction,
 	TSuccessCallback<FString> OnSuccess, FFailureCallback OnFailure)
 {
-	//FString ID = Transaction.ID();
-	//SendTransaction(Transaction, [=](FHash256 Hash)
-	//{
-	//	OnSuccess(ID);
-//	}, OnFailure);
+	FString ID = Transaction.ID();
+	SendTransaction(Transaction, [=](FHash256 Hash)
+	{
+		OnSuccess(ID);
+	}, OnFailure);
 }
 
 //appending functions from sequenceData.cpp
