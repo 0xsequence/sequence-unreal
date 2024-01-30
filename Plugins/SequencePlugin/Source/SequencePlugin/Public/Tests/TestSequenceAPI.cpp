c2c22f8508c85bef99a668ce3c75280365c74162
CalvinKork <calvin.kork@zemind.ca>
2024-01-30 12:30:41 -0600

    Updated the testing flow for the SequenceAPI making it much easier to test all the calls

--- Plugins/SequencePlugin/Source/SequencePlugin/Public/Tests/TestSequenceAPI.cpp
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Tests/TestSequenceAPI.cpp
@@ -3,9 +3,69 @@
 #include "Sequence/SequenceAPI.h"
 #include "Authenticator.h"
 
-void SequenceAPITest::RunTest(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure)
+void SequenceAPITest::RegisterSession(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure)
 {
-	UAuthenticator * Auth = NewObject<UAuthenticator>();
+	const UAuthenticator * Auth = NewObject<UAuthenticator>();
+	USequenceWallet * Api = USequenceWallet::Make(Auth->GetStoredCredentials().GetCredentials());
+
+	const TFunction<void(FString)> OnResponse = [OnSuccess](FString Response)
+	{
+		OnSuccess("RegisterSession Test Passed");	
+	};
+	
+	const FFailureCallback GenericFailure = [OnFailure](const FSequenceError& Error)
+	{
+		OnFailure("Test Failed", Error);
+	};
+	
+	UE_LOG(LogTemp,Display,TEXT("========================[Running Sequence API RegisterSession Test]========================"));
+
+	Api->RegisterSession(OnSuccess,GenericFailure);
+}
+
+void SequenceAPITest::SignMessage(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure)
+{
+	const UAuthenticator * Auth = NewObject<UAuthenticator>();
+	USequenceWallet * Api = USequenceWallet::Make(Auth->GetStoredCredentials().GetCredentials());
+
+	const TSuccessCallback<FSignedMessage> OnResponse = [OnSuccess] (FSignedMessage response)
+	{
+		OnSuccess("Sign Message Test Passed");
+	};
+	
+	const FFailureCallback GenericFailure = [OnFailure](const FSequenceError& Error)
+	{
+		OnFailure("Sign Message Test Failed", Error);
+	};
+	
+	UE_LOG(LogTemp,Display,TEXT("========================[Running Sequence API SignMessage Test]========================"));
+
+	Api->SignMessage("hi",OnResponse,GenericFailure);
+}
+
+void SequenceAPITest::ListSessions(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure)
+{
+	const UAuthenticator * Auth = NewObject<UAuthenticator>();
+	USequenceWallet * Api = USequenceWallet::Make(Auth->GetStoredCredentials().GetCredentials());
+
+	const TSuccessCallback<TArray<FSession>> OnResponse = [OnSuccess](TArray<FSession> response)
+	{
+		OnSuccess("List Sessions Test Passed");
+	};
+	
+	const FFailureCallback GenericFailure = [OnFailure](const FSequenceError& Error)
+	{
+		OnFailure("Test Failed", Error);
+	};
+	
+	UE_LOG(LogTemp,Display,TEXT("========================[Running Sequence API ListSessions Test]========================"));
+
+	Api->ListSessions(OnResponse,GenericFailure);
+}
+
+void SequenceAPITest::SendTransaction(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure)
+{
+	const UAuthenticator * Auth = NewObject<UAuthenticator>();
 	USequenceWallet * Api = USequenceWallet::Make(Auth->GetStoredCredentials().GetCredentials());
 
 	const FFailureCallback GenericFailure = [OnFailure](const FSequenceError& Error)
@@ -13,10 +73,28 @@ void SequenceAPITest::RunTest(TFunction<void(FString)> OnSuccess, TFunction<void
 		OnFailure("Test Failed", Error);
 	};
 	
-	UE_LOG(LogTemp,Display,TEXT("========================[Running Sequence API Tests]========================"));
+	UE_LOG(LogTemp,Display,TEXT("========================[Running Sequence API SendTransaction Test]========================"));
+	
+	//TArray<FRawTransaction> Txn;
+	//Api->SendTransaction(OnSuccess,GenericFailure);
+}
 
-	Api->RegisterSession(OnSuccess,GenericFailure);
-	//Api->ListSessions(OnSuccess,GenericFailure);
-	//Api->SignMessage("hi",OnSuccess,GenericFailure);
-	//Api->CloseSession(OnSuccess,GenericFailure);
+void SequenceAPITest::CloseSession(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure)
+{
+	const UAuthenticator * Auth = NewObject<UAuthenticator>();
+	USequenceWallet * Api = USequenceWallet::Make(Auth->GetStoredCredentials().GetCredentials());
+
+	const TFunction<void(void)> OnResponse = [OnSuccess]()
+	{
+		OnSuccess("CloseSession Test Passed");
+	};
+	
+	const FFailureCallback GenericFailure = [OnFailure](const FSequenceError& Error)
+	{
+		OnFailure("Test Failed", Error);
+	};
+	
+	UE_LOG(LogTemp,Display,TEXT("========================[Running Sequence API CloseSession Test]========================"));
+
+	Api->CloseSession(OnResponse,GenericFailure);
 }
--- Plugins/SequencePlugin/Source/SequencePlugin/Public/Tests/TestSequenceAPI.h
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Tests/TestSequenceAPI.h
@@ -4,5 +4,9 @@
 class SequenceAPITest
 {
 public:
-	static void RunTest(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
+	static void RegisterSession(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
+	static void SignMessage(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
+	static void ListSessions(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
+	static void SendTransaction(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
+	static void CloseSession(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
 };
