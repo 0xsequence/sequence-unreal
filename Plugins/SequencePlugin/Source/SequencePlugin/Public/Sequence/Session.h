70f0cc37f8a1b5ee4c8bd8d41d26fc207c7d5be2
CalvinKork <calvin.kork@zemind.ca>
2024-01-30 08:49:47 -0600

    Resolving some tester conflicts

---
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Sequence/Session.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Session.generated.h"
+
+/**
+ * 
+ */
+USTRUCT()
+struct FSession
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FString Id;
+	UPROPERTY()
+	FString Address;
+	UPROPERTY()
+	FString UserID;
+	UPROPERTY()
+	FString ProjectID;
+	UPROPERTY()
+	FString Issuer;
+	UPROPERTY()
+	FString Subject;
+	UPROPERTY()
+	FString FriendlyName;
+	UPROPERTY()
+	FString CreatedAt;
+	UPROPERTY()
+	FString RefreshedAt;
+	UPROPERTY()
+	FString ExpiresAt;
+};
