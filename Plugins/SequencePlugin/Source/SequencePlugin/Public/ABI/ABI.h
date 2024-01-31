9c511fece38847036565630740a46d8ce074d12e
Jan D <jandohring@hotmail.com>
2024-01-31 14:44:57 -0500

    abi

--- Plugins/SequencePlugin/Source/SequencePlugin/Public/ABI/ABI.h
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/ABI/ABI.h
@@ -1,63 +1,71 @@
 #pragma once
 #include "Types/BinaryData.h"
-#include "Types/Types.h"
-#include "ABI/ABITypes.h"
 
-inline uint8 GMethodIdByteLength = 4;
-inline uint8 GBlockByteLength = 32;
-uint8* NewEmptyBlock();
+static constexpr int GBlockByteLength = 32;
+static constexpr int GSignatureLength = 4;
 
-enum EABIArgType
-{
-	STATIC, // Integers, bytes, and hashes
-	BYTES, 
-	STRING,
-	ARRAY,
-	FIXED // Like array, but no size attribute
-	//Address,
-};
-
-FString TypeToString(EABIArgType Type);	
-
-struct FABIArg
-{
-	EABIArgType Type;
-	uint32 Length; // For RAW types, this is the number of bytes. For Arrays, this is the number of entries.
-	void* Data;
-
-	uint8 GetBlockNum() const;
-	void Encode(uint8* Start, uint8** Head, uint8** Tail);
-	FABIArg Decode(uint8* TrueStart, uint8* Start, uint8* Head);
-	FUnsizedData ToBinary() const;
-	FString ToHex() const;
-	FString ToString() const;
-	uint8 ToUint8() const;
-	FABIArg* ToArr() const;
-	void Log();
-
-	// Free Data
-	void Destroy();
-
-	static FABIArg Empty(EABIArgType Type); // initializes it without data
-
-	// Creates data; must be destroyed
-	static FABIArg New(FString Value);
-	static FABIArg New(uint8 Value);
-	static FABIArg New(uint32 Value);
-	static FABIArg New(FABIArg* Array, uint32 Length); 
-};
-
-class ABI
+class ABIEncodeable
 {
 public:
-	static void ParseMethod(FString& Method);
-	static FUnsizedData Encode(FString Method, TArray<FABIProperty*> &Args);
-	static FUnsizedData EncodeArgs(FString Method, FABIArg* Args, uint8 ArgNum);
-	static FUnsizedData EncodeArgsWithoutSignature(FABIArg* Args, uint8 ArgNum);
-	static void DecodeArgs(FUnsizedData Data, FABIArg* Args, uint8 ArgNum);
-	static void DecodeArgsWithoutSignature(FUnsizedData Data, FABIArg* Args, uint8 ArgNum);
-	static void Decode(FUnsizedData Data, TArray<FABIProperty*> &Args);
+	virtual ~ABIEncodeable() = default;
+	virtual void EncodeHead(TArray<uint8> &Data) = 0;
+	/*
+	 * The HeadPosition is 
+	 */
+	virtual void EncodeTail(TArray<uint8> &Data, int HeadPosition, int Offset) = 0;
+	static void PushEmptyBlock(TArray<uint8> &Data);
+	static void CopyInUInt32(TArray<uint8> &Data, uint32 Value, int BlockPosition);
+	static void CopyInInt32(TArray<uint8> &Data, int32 Value, int BlockPosition);
 };
 
-void CopyInUint32(uint8* BlockPtr, uint32 Value);
-uint32 CopyOutUint32(uint8* BlockPtr);
+class TFixedABIArray : public ABIEncodeable
+{
+	TArray<ABIEncodeable*> MyData;
+public:
+	TFixedABIArray();
+	TFixedABIArray(TArray<ABIEncodeable*> MyData);
+	TArray<uint8> Encode();
+	virtual void EncodeHead(TArray<uint8> &Data) override;
+	virtual void EncodeTail(TArray<uint8> &Data, int HeadPosition, int Offset) override;
+	void Push(ABIEncodeable* Arg);
+};
+
+class TDynamicABIArray : public ABIEncodeable
+{
+	TArray<ABIEncodeable*> MyData;
+public:
+	TDynamicABIArray();
+	TDynamicABIArray(TArray<ABIEncodeable*> MyData);
+	virtual void EncodeHead(TArray<uint8> &Data) override;
+	virtual void EncodeTail(TArray<uint8> &Data, int HeadPosition, int Offset) override;
+	void Push(ABIEncodeable* Arg);
+};
+
+class TFixedABIData : public ABIEncodeable
+{
+	TArray<uint8> MyData;
+public:
+	TFixedABIData(TArray<uint8> MyData);
+	virtual void EncodeHead(TArray<uint8> &Data) override;
+	virtual void EncodeTail(TArray<uint8> &Data, int HeadPosition, int Offset) override;
+};
+
+class TDynamicABIData : public ABIEncodeable
+{
+	TArray<uint8> MyData;
+public:
+	TDynamicABIData(TArray<uint8> MyData);
+	virtual void EncodeHead(TArray<uint8> &Data) override;
+	virtual void EncodeTail(TArray<uint8> &Data, int HeadPosition, int Offset) override;
+};
+
+class ABI : public ABIEncodeable
+{
+public:
+	static TFixedABIData UInt32(uint32 Input);
+	static TFixedABIData Int32(int32 Input);
+	static TFixedABIData Bool(bool Input);
+	static TDynamicABIData String(FString Input);
+	static FUnsizedData Encode(FString Signature, TArray<ABIEncodeable*> Arr);
+	static FString Display(FString Signature, TArray<ABIEncodeable*> Arr);
+};
