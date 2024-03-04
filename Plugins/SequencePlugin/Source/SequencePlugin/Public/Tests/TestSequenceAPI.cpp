f869ec2261f299b4f9a8745926231c0e144a6171
CalvinKork <calvin.kork@zemind.ca>
2024-03-04 08:55:38 -0600

    Removed old sending test that was being un used

--- Plugins/SequencePlugin/Source/SequencePlugin/Public/Tests/TestSequenceAPI.cpp
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Tests/TestSequenceAPI.cpp
@@ -220,54 +220,6 @@ void SequenceAPITest::SendERC1155(TFunction<void(FString)> OnSuccess, TFunction<
 	},GenericFailure);
 }
 
-void SequenceAPITest::SendTransaction(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure)
-{
-	const UAuthenticator * Auth = NewObject<UAuthenticator>();
-	USequenceWallet * Api = USequenceWallet::Make(Auth->GetStoredCredentials().GetCredentials());
-
-	const FFailureCallback GenericFailure = [OnFailure](const FSequenceError& Error)
-	{
-		OnFailure("Test Failed", Error);
-	};
-	
-	UE_LOG(LogTemp,Display,TEXT("========================[Running Sequence API SendTransaction Test]========================"));
-	
-	TArray<TUnion<FRawTransaction,FERC20Transaction,FERC721Transaction,FERC1155Transaction>> Txn;
-	FRawTransaction T;
-	T.data = "data?";
-	T.to = "0x0E0f9d1c4BeF9f0B8a2D9D4c09529F260C7758A2";
-	T.value = "0";
-	
-	FERC20Transaction T20;
-	T20.to = "0x0E0f9d1c4BeF9f0B8a2D9D4c09529F260C7758A2";
-	T20.token = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
-	T20.value = "100000";
-
-	FERC721Transaction T721;
-	T721.safe = true;
-	T721.id = "54530968763798660137294927684252503703134533114052628080002308208148824588621";
-	T721.to = "0x0E0f9d1c4BeF9f0B8a2D9D4c09529F260C7758A2";
-	T721.token = "0xa9a6A3626993D487d2Dbda3173cf58cA1a9D9e9f";
-
-	FERC1155Transaction T1155;
-	T1155.to = "0x0E0f9d1c4BeF9f0B8a2D9D4c09529F260C7758A2";
-	T1155.token = "0x631998e91476DA5B870D741192fc5Cbc55F5a52E";
-	
-	FERC1155TxnValue val;
-	val.amount = "1";
-	val.id = "66635";
-	T1155.vals.Add(val);
-	
-	//Txn.Push(TUnion<FRawTransaction,FERC20Transaction,FERC721Transaction,FERC1155Transaction>(T));
-	//Txn.Push(TUnion<FRawTransaction,FERC20Transaction,FERC721Transaction,FERC1155Transaction>(T20));
-	//Txn.Push(TUnion<FRawTransaction,FERC20Transaction,FERC721Transaction,FERC1155Transaction>(T721));
-	//Txn.Push(TUnion<FRawTransaction,FERC20Transaction,FERC721Transaction,FERC1155Transaction>(T1155));
-
-	//Api->SendTransaction(Txn, OnSuccess, GenericFailure);
-	
-	//Api->SendTransaction(Txn,OnSuccess,GenericFailure);
-}
-
 void SequenceAPITest::CloseSession(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure)
 {
 	const UAuthenticator * Auth = NewObject<UAuthenticator>();
--- Plugins/SequencePlugin/Source/SequencePlugin/Public/Tests/TestSequenceAPI.h
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Tests/TestSequenceAPI.h
@@ -7,7 +7,6 @@ public:
 	static void RegisterSession(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
 	static void SignMessage(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
 	static void ListSessions(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
-	static void SendTransaction(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
 	static void SendRaw(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
 	static void CallContract(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
 	static void SendERC20(TFunction<void(FString)> OnSuccess, TFunction<void(FString, FSequenceError)> OnFailure);
