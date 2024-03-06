045366ec02c7e395e8dff65b5188db99bcf54545
CalvinKork <calvin.kork@zemind.ca>
2024-03-06 12:33:35 -0600

    Introduced Config file for Waas

---
+++ Plugins/SequencePlugin/Source/SequencePlugin/Private/Config/Config.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+
+struct FSequenceConfig
+{	
+	inline static FString VITE_SEQUENCE_WAAS_CONFIG_KEY = "eyJwcm9qZWN0SWQiOjIsImVtYWlsUmVnaW9uIjoidXMtZWFzdC0yIiwiZW1haWxDbGllbnRJZCI6IjVncDltaDJmYnFiajhsNnByamdvNzVwMGY2IiwicnBjU2VydmVyIjoiaHR0cHM6Ly9uZXh0LXdhYXMuc2VxdWVuY2UuYXBwIn0=";
+
+	inline static FString ProjectAccessKey = "EeP6AmufRFfigcWaNverI6CAAAAAAAAAA";
+
+	inline static FString WaasVersion = "1.0.0";
+};
