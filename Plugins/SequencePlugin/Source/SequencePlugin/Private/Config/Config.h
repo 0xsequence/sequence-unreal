e679748d8d03358ac1438e383cdfe8c010b2bc09
CalvinKork <calvin.kork@zemind.ca>
2024-03-08 14:14:56 -0600

    More updates to docs, GoogleSignin works

--- Plugins/SequencePlugin/Source/SequencePlugin/Private/Config/Config.h
+++ Plugins/SequencePlugin/Source/SequencePlugin/Private/Config/Config.h
@@ -9,3 +9,22 @@ struct FSequenceConfig
 
 	inline static FString WaasVersion = "1.0.0";
 };
+
+struct FAuthenticatorConfig
+{
+	//inline static FString DeepLinkURLScheme = "sdk-powered-by-sequence";
+	inline static FString UrlScheme = "powered-by-sequence";
+	inline static FString RedirectURL = "https://3d41-142-115-54-118.ngrok-free.app/";
+
+	inline static FString GoogleAuthURL = "https://accounts.google.com/o/oauth2/auth";
+	inline static FString GoogleClientID = "970987756660-35a6tc48hvi8cev9cnknp0iugv9poa23.apps.googleusercontent.com";
+
+	inline static FString FacebookAuthURL = "https://www.facebook.com/v18.0/dialog/oauth";
+	inline static FString FacebookClientID = "";//TODO still need this
+
+	inline static FString DiscordAuthURL = "https://discord.com/api/oauth2/authorize";
+	inline static FString DiscordClientID = "";//TODO still need this
+
+	inline static FString AppleAuthURL = "https://appleid.apple.com/auth/authorize";
+	inline static FString AppleClientID = "com.horizon.sequence.waas";
+};
