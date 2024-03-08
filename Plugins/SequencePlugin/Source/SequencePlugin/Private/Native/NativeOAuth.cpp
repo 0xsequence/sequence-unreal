438af35b0a1553e1a0d2c285e93477146ac55168
CalvinKork <calvin.kork@zemind.ca>
2024-03-08 09:33:19 -0600

    fix 1

---
+++ Plugins/SequencePlugin/Source/SequencePlugin/Private/Native/NativeOAuth.cpp
@@ -0,0 +1,12 @@
+#if PLATFORM_ANDROID
+#include "Native/Android/AndroidOAuth.h"
+#endif // PLATFORM_ANDROID
+
+namespace NativeOAuth {
+    void RequestAuthCode(const FString& providerUrl, const FString& redirectScheme)
+    {
+        #if PLATFORM_ANDROID
+        AndroidOAuth::AndroidThunkCpp_RequestAuthCode(providerUrl, redirectScheme);
+        #endif // PLATFORM_ANDROID
+    }
+}
--- Plugins/SequencePlugin/Source/SequencePlugin/Public/Authenticator.h
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Authenticator.h
@@ -320,6 +320,7 @@ private:
 	UPROPERTY()
 	FString Nonce = "";
 
+	const FString DeepLinkURLScheme = "sdk-powered-by-sequence";
 	const FString UrlScheme = "powered-by-sequence";
 	const FString RedirectURL = "https://3d41-142-115-54-118.ngrok-free.app/";
 
@@ -381,7 +382,7 @@ private:
 
 	void ResetRetryEmailLogin();
 
-	FString GenerateSigninURL(const FString& AuthURL, const FString& ClientID) const;
+	FString GenerateSigninURL(const FString& AuthURL, const FString& ClientID, const FString& Method) const;
 
 	FString BuildAWSURL(const FString& Service, const FString& AWSRegion);
 
