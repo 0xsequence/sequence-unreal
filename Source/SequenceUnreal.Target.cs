bbf640c2acf9a9b8cb3c492bb27c18441b5ce20b
CalvinKork <calvin.kork@zemind.ca>
2024-03-15 10:08:31 -0500

    Clearing typo in URL, updating build targets to clear warnings

--- Source/SequenceUnreal.Target.cs
+++ Source/SequenceUnreal.Target.cs
@@ -1,15 +1,14 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class SequenceUnrealTarget : TargetRules
 {
 	public SequenceUnrealTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
 		ExtraModuleNames.Add("SequenceUnreal");
 	}
 }
--- Source/SequenceUnrealEditor.Target.cs
+++ Source/SequenceUnrealEditor.Target.cs
@@ -1,15 +1,14 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class SequenceUnrealEditorTarget : TargetRules
 {
 	public SequenceUnrealEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
 		ExtraModuleNames.Add("SequenceUnreal");
 	}
 }
