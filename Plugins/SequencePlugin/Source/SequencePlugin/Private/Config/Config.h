00616a70181c39be04413ca7dc1eecc2ec40ba23
CalvinKork <calvin.kork@zemind.ca>
2024-04-04 08:14:48 -0500

    Cleaning up Config.h

--- Plugins/SequencePlugin/Source/SequencePlugin/Private/Config/Config.h
+++ Plugins/SequencePlugin/Source/SequencePlugin/Private/Config/Config.h
@@ -14,18 +14,16 @@ struct FSequenceConfig
 struct FAuthenticatorConfig
 {
 	inline static FString UrlScheme = "powered-by-sequence";
-	//https://dev2-api.sequence.app/oauth/callback
-	//inline static FString RedirectURL = "https://0xsequence.github.io/demo-waas-auth/";
 	inline static FString RedirectURL = "https://dev2-api.sequence.app/oauth/callback";
 
 	inline static FString GoogleAuthURL = "https://accounts.google.com/o/oauth2/auth";
 	inline static FString GoogleClientID = "970987756660-35a6tc48hvi8cev9cnknp0iugv9poa23.apps.googleusercontent.com";
 
 	inline static FString FacebookAuthURL = "https://www.facebook.com/v18.0/dialog/oauth";
-	inline static FString FacebookClientID = "";//TODO still need this
+	inline static FString FacebookClientID = "";
 
 	inline static FString DiscordAuthURL = "https://discord.com/api/oauth2/authorize";
-	inline static FString DiscordClientID = "";//TODO still need this
+	inline static FString DiscordClientID = "";
 
 	inline static FString AppleAuthURL = "https://appleid.apple.com/auth/authorize";
 	inline static FString AppleClientID = "com.horizon.sequence.waas";
