5ac68ff9b4f920853ea684f5077aa3ac5b8a7a6b
CalvinKork <calvin.kork@zemind.ca>
2024-05-10 14:54:59 -0500

    Initial working windows encryptor

--- Plugins/SequencePlugin/Source/SequencePlugin/Private/NativeEncryptors/WindowsEncryptor.cpp
+++ Plugins/SequencePlugin/Source/SequencePlugin/Private/NativeEncryptors/WindowsEncryptor.cpp
@@ -1,6 +1,8 @@
 //Copyright 2024 Horizon Blockchain Games Inc. All rights reserved.
 
 #include "NativeEncryptors/WindowsEncryptor.h"
+#include "Types/BinaryData.h"
+#include "Util/HexUtility.h"
 
 #if PLATFORM_WINDOWS
 #include "Windows/WindowsHWrapper.h"
@@ -11,41 +13,44 @@ FString UWindowsEncryptor::Encrypt(const FString& StringIn)
 {
 	FString Result = "";
 	UE_LOG(LogTemp,Display,TEXT("Preparing to encrypt on windows: %s"),*StringIn);
+	UE_LOG(LogTemp,Display,TEXT("InLength: %d"),StringIn.Len());
 #if PLATFORM_WINDOWS
 	DATA_BLOB DataIn;
 	DATA_BLOB DataOut;
+	const FString ProcString = FBase64::Encode(StringIn);
 
-	//Copy in our FString data into a Char* array
-	const int32 InSize = StringIn.GetCharArray().Num();
-	char * CharsIn =  new char[InSize];
-	for (int i = 0; i < InSize; i++)
-		CharsIn[i] = StringIn.GetCharArray()[i];
+	UE_LOG(LogTemp,Display,TEXT("Decrypted PreResult: %s"),*ProcString);
+	UE_LOG(LogTemp,Display,TEXT("PreResult Length: %d"),ProcString.Len());
 	
-	BYTE *pbDataInput = (BYTE *)CharsIn;
-	const DWORD cbDataInput = strlen(CharsIn);
+	const int32 InSize = ProcString.Len();
+	uint8 * CharsIn =  new uint8[InSize];
+	StringToBytes(ProcString,CharsIn,InSize);
+	BYTE *pbDataInput = CharsIn;
+	const DWORD cbDataInput = InSize;
+	UE_LOG(LogTemp,Display,TEXT("InByteCount: %d"),cbDataInput);
+	
 	DataIn.pbData = pbDataInput;
 	DataIn.cbData = cbDataInput;
 	if (CryptProtectData(
 		&DataIn,
-		L"EncryptedPayload",
+		NULL,
 		NULL,
 		NULL,
 		NULL,
 		0,
 		&DataOut))
 	{
-		const int32 OutSize = DataOut.cbData;		
-		for (int32 i = 0; i < OutSize; i++)
-			Result.AppendChar(DataOut.pbData[i]);
+		BytesToHex(DataOut.pbData,DataOut.cbData,Result);
 		LocalFree(DataOut.pbData);
 	}
 	else
 	{
 		UE_LOG(LogTemp,Display,TEXT("Encryption Failed on windows"));
 	}
-	delete[] CharsIn;
+	//delete[] CharsIn;
 #endif
 	UE_LOG(LogTemp,Display,TEXT("Encrypted Result: %s"),*Result);
+	UE_LOG(LogTemp,Display,TEXT("OutLength: %d"),Result.Len());
 	return Result;
 }
 
@@ -53,18 +58,50 @@ FString UWindowsEncryptor::Decrypt(const FString& StringIn)
 {
 	FString Result = "";
 	UE_LOG(LogTemp,Display,TEXT("Preparing to decrypt on windows: %s"),*StringIn);
+	UE_LOG(LogTemp,Display,TEXT("InLength: %d"),StringIn.GetCharArray().Num());
 #if PLATFORM_WINDOWS
 	DATA_BLOB DataIn;
 	DATA_BLOB DataOut;
 	
 	//Copy in our FString data into a Char* array
-	const int32 InSize = StringIn.GetCharArray().Num();
+	/*const int32 InSize = StringIn.GetCharArray().Num();
 	char * CharsIn =  new char[InSize];
 	for (int i = 0; i < InSize; i++)
-		CharsIn[i] = StringIn.GetCharArray()[i];
+		CharsIn[i] = (char)StringIn.GetCharArray()[i];*/
+
+	//char * InBytes = TCHAR_TO_ANSI(*StringIn);
+	/*const char * InBytes = StringCast<ANSICHAR>(*StringIn).Get();
+	const BYTE *pbDataInput = reinterpret_cast<const BYTE*>(InBytes);
 	
-	BYTE *pbDataInput = (BYTE *)CharsIn;
-	const DWORD cbDataInput = StringIn.GetCharArray().Num();
+	//const DWORD cbDataInput = StringIn.Len() * sizeof(TCHAR);
+	const DWORD cbDataInput = strlen((char *)pbDataInput);
+	UE_LOG(LogTemp,Display,TEXT("InByteCount: %d"),cbDataInput);*/
+
+	/*const FUnsizedData InBytes = StringToUTF8(StringIn);
+	BYTE *pbDataInput = InBytes.Arr->GetData();
+	const DWORD cbDataInput = strlen((char *)pbDataInput)+1;
+	UE_LOG(LogTemp,Display,TEXT("InByteCount: %d"),cbDataInput);*/
+
+	/*const char * InBytes = StringCast<ANSICHAR>(*StringIn).Get();
+	const BYTE *pbDataInput = reinterpret_cast<const BYTE*>(InBytes);
+	const DWORD cbDataInput = strlen((char *)pbDataInput);
+	UE_LOG(LogTemp,Display,TEXT("InByteCount: %d"),cbDataInput);*/
+
+	//Copy in our FString data into a Char* array
+
+	const int32 InSize = StringIn.Len() / 2;
+	uint8 * CharsIn = new uint8[InSize];
+	HexToBytes(StringIn,CharsIn);
+	
+	/*const int32 InSize = StringIn.GetCharArray().Num();
+	char * CharsIn =  new char[InSize];
+	for (int i = 0; i < InSize; i++)
+		CharsIn[i] = static_cast<char>(StringIn.GetCharArray()[i]);*/
+	
+	BYTE *pbDataInput = CharsIn;
+	const DWORD cbDataInput = InSize;
+	UE_LOG(LogTemp,Display,TEXT("InByteCount: %d"),cbDataInput);
+	
 	DataIn.pbData = pbDataInput;
 	DataIn.cbData = cbDataInput;
 	LPWSTR pDescrOut = NULL;
@@ -77,17 +114,33 @@ FString UWindowsEncryptor::Decrypt(const FString& StringIn)
 		0,
 		&DataOut))
 	{
-		const int32 OutSize = DataOut.cbData;		
-		for (int32 i = 0; i < OutSize; i++)
-			Result.AppendChar(DataOut.pbData[i]);
+		//const char * OutBytes = reinterpret_cast<char *>(DataOut.pbData);
+		//const int32 OutSize = DataOut.cbData;
+
+		const FString PreResult = BytesToString(DataOut.pbData,DataOut.cbData);
+
+		UE_LOG(LogTemp,Display,TEXT("Decrypted PreResult: %s"),*PreResult);
+		UE_LOG(LogTemp,Display,TEXT("PreResult Length: %d"),PreResult.Len());
+		
+		if (FBase64::Decode(PreResult,Result))
+		{
+			UE_LOG(LogTemp,Display,TEXT("Successful B64 Decode"));
+		}
+		else
+		{
+			UE_LOG(LogTemp,Error,TEXT("UnSuccessful B64 Decode"));
+		}
 		
 		LocalFree(DataOut.pbData);
+		LocalFree(pDescrOut);
 	}
 	else
 	{
-		UE_LOG(LogTemp,Display,TEXT("Decryption Failed on windows"));
+		const int32 ErrorCode = GetLastError();
+		UE_LOG(LogTemp,Display,TEXT("Decryption Failed on windows, Error code: %d"),ErrorCode);
 	}
 #endif
 	UE_LOG(LogTemp,Display,TEXT("Decrypted Result: %s"),*Result);
+	UE_LOG(LogTemp,Display,TEXT("OutLength: %d"),Result.Len());
 	return Result;
 }
