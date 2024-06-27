55ca2b02577fecdbfb5ceff9dd5610dc0237b6e4
Quinn Purdy <qp@horizon.io>
2024-06-27 08:53:37 -0400

    Lower case emails before doing email auth. We noticed in sequence.js that this could create duplicate accounts with different email cases

--- Plugins/SequencePlugin/Source/SequencePlugin/Private/Authenticator.cpp
+++ Plugins/SequencePlugin/Source/SequencePlugin/Private/Authenticator.cpp
@@ -265,6 +265,7 @@ void UAuthenticator::EmailLogin(const FString& EmailIn)
 	{
 		this->ResetRetryEmailLogin();
 		this->Cached_Email = EmailIn;
+		EmailIn = EmailIn.ToLower();
 		CognitoIdentityInitiateAuth(this->Cached_Email,this->WaasSettings.GetEmailClientId());
 	}
 	else
@@ -425,7 +426,7 @@ void UAuthenticator::ProcessCognitoIdentityInitiateAuth(FHttpRequestPtr Req, FHt
 void UAuthenticator::CognitoIdentityInitiateAuth(const FString& Email, const FString& AWSCognitoClientID)
 {
 	const FString URL = BuildAWSURL("cognito-idp",this->WaasSettings.GetEmailRegion());
-	const FString RequestBody = "{\"AuthFlow\":\"CUSTOM_AUTH\",\"AuthParameters\":{\"USERNAME\":\""+ Email +"\"},\"ClientId\":\""+ AWSCognitoClientID +"\"}";
+	const FString RequestBody = "{\"AuthFlow\":\"CUSTOM_AUTH\",\"AuthParameters\":{\"USERNAME\":\""+ Email.ToLower() +"\"},\"ClientId\":\""+ AWSCognitoClientID +"\"}";
 	
 	if (this->CanRetryEmailLogin())
 	{
@@ -458,6 +459,7 @@ void UAuthenticator::ProcessCognitoIdentitySignUp(FHttpRequestPtr Req, FHttpResp
 void UAuthenticator::CognitoIdentitySignUp(const FString& Email, const FString& Password, const FString& AWSCognitoClientID)
 {
 	const FString URL = BuildAWSURL("cognito-idp",this->WaasSettings.GetEmailRegion());
+	Email = Email.ToLower();
 	const FString RequestBody = "{\"ClientId\":\""+ AWSCognitoClientID +"\",\"Password\":\""+ Password +"\",\"UserAttributes\":[{\"Name\":\"email\",\"Value\":\""+ Email +"\"}],\"Username\":\""+ Email +"\"}";
 	this->UEAmazonWebServerRPC(URL,RequestBody, "AWSCognitoIdentityProviderService.SignUp",&UAuthenticator::ProcessCognitoIdentitySignUp);
 }
@@ -493,6 +495,7 @@ void UAuthenticator::ProcessAdminRespondToAuthChallenge(FHttpRequestPtr Req, FHt
 
 void UAuthenticator::AdminRespondToAuthChallenge(const FString& Email, const FString& Answer, const FString& ChallengeSessionString, const FString& AWSCognitoClientID)
 {
+	Email = Email.ToLower();
 	const FString URL = BuildAWSURL("cognito-idp",this->WaasSettings.GetEmailRegion());
 	const FString RequestBody = "{\"ChallengeName\":\"CUSTOM_CHALLENGE\",\"ClientId\":\""+ AWSCognitoClientID +"\",\"Session\":\""+ ChallengeSessionString +"\",\"ChallengeResponses\":{\"USERNAME\":\""+ Email +"\",\"ANSWER\":\""+ Answer +"\"},\"ClientMetadata\":{\"SESSION_HASH\":\""+this->SessionHash+"\"}}";
 	this->UEAmazonWebServerRPC(URL, RequestBody,"AWSCognitoIdentityProviderService.RespondToAuthChallenge",&UAuthenticator::ProcessAdminRespondToAuthChallenge);
