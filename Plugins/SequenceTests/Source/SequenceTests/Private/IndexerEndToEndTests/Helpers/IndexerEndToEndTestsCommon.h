dc1758ec4385d10e99aa43ff7422a4ba98bde114
Quinn Purdy <qp@horizon.io>
2024-09-24 14:58:29 -0400

    Added GetTokenSupplies test and fixed the args such that omitting a page will cause the page to be omitted from the JSON as opposed to being included with default values - this resolves a bug where we got no responses.

--- Plugins/SequenceTests/Source/SequenceTests/Private/IndexerEndToEndTests/Helpers/IndexerEndToEndTestsCommon.h
+++ Plugins/SequenceTests/Source/SequenceTests/Private/IndexerEndToEndTests/Helpers/IndexerEndToEndTestsCommon.h
@@ -7,5 +7,6 @@
 namespace IndexerEndToEndTestsCommon
 {
 	const FString TestAddress = TEXT("0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8B9"); // From the Sequence Docs - this address has a fair amount of tokens held on Polygon and works great for our test cases
-	const int64 PolygonNetworkId = 137; 
+	const int64 PolygonNetworkId = 137;
+	const FString TestContractAddress_Skyweaver = TEXT("0x631998e91476DA5B870D741192fc5Cbc55F5a52E");
 }
