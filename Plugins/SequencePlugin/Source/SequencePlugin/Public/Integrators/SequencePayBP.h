eddcf81d72993e0b97a2143353431953adec2916
Andy Grüning <andygruening@gmail.com>
2025-01-19 14:42:24 +0100

    added SequencePayBP subsystem with on ramp functions

---
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Integrators/SequencePayBP.h
@@ -0,0 +1,28 @@
+// Copyright 2024 Horizon Blockchain Games Inc. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Sequence/SequenceWalletBPTypes.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "UObject/Object.h"
+#include "SequencePayBP.generated.h"
+
+DECLARE_DYNAMIC_DELEGATE_OneParam(FOnSupportedTransakCountries, const TArray<FSupportedCountry>&, SupportedCountries);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FOnFailure, const FString&, Error);
+
+UCLASS()
+class SEQUENCEPLUGIN_API USequencePayBP : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="0xSequence SDK - Functions")
+	FString GetOnRampLink(const FString& WalletAddress, const FTransakSettings& Settings);
+	
+	UFUNCTION(BlueprintCallable, Category="0xSequence SDK - Functions")
+	void OpenOnRampInExternalBrowser(const FString& WalletAddress, const FTransakSettings& Settings);
+
+	UFUNCTION(BlueprintCallable, Category="0xSequence SDK - Functions")
+	void GetSupportedOnRampCountries(FOnSupportedTransakCountries OnSuccess, FOnFailure OnFailure);
+};
