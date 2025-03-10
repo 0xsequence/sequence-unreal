95d8ddf8adf2c94e418855d11bab23b31196a07a
Jan Dohring <114616439+ZemindJan@users.noreply.github.com>
2025-03-10 16:27:43 -0400

    full api

---
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Marketplace/Sardine/Structs/SardineEnabledToken.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SardineToken.h"
+#include "SardineEnabledToken.generated.h"
+
+USTRUCT(BlueprintType)
+struct SEQUENCEPLUGIN_API FSardineEnabledToken : public FSardineToken
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	
+};
---
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Marketplace/Sardine/Structs/SardineSupportedToken.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SardineToken.h"
+#include "SardineSupportedToken.generated.h"
+
+USTRUCT(BlueprintType)
+struct SEQUENCEPLUGIN_API FSardineSupportedToken : public FSardineToken
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	
+};
