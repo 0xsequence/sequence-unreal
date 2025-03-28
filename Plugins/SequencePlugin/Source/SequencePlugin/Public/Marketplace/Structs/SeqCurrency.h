b4874f10c963491b42a05d8a569b57cc157a7bd2
Jan Dohring <jan.dohring@zemind.ca>
2025-03-28 13:07:13 -0400

    Currency updated

--- Plugins/SequencePlugin/Source/SequencePlugin/Public/Marketplace/Structs/SeqCurrency.h
+++ Plugins/SequencePlugin/Source/SequencePlugin/Public/Marketplace/Structs/SeqCurrency.h
@@ -11,7 +11,7 @@ struct SEQUENCEPLUGIN_API FSeqCurrency
 public:
 	// Properties
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	int ID;
+	u_long ID;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int ChainID;
@@ -19,10 +19,46 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FString ContractAddress;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Symbol;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	u_long Decimals;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString ImageURL;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	double ExchangeRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool DefaultChainCurrency;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString CreatedAt;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString UpdatedAt;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString DeletedAt;
+
 	void Setup(FJsonObject json_in)
 	{
-		ID = json_in.GetIntegerField(TEXT("id"));
+		ID = json_in.GetNumberField(TEXT("id"));
 		ChainID = json_in.GetIntegerField(TEXT("chain_id"));
-		ContractAddress = json_in.GetStringField(TEXT("contract_address"));	
+		ContractAddress = json_in.GetStringField(TEXT("contract_address"));
+		Name = json_in.GetStringField(TEXT("name"));
+		Symbol = json_in.GetStringField(TEXT("symbol"));
+		Decimals = json_in.GetNumberField(TEXT("decimals"));
+		ImageURL = json_in.GetStringField(TEXT("image_url"));
+		ExchangeRate = json_in.GetNumberField(TEXT("exchange_rate"));
+		DefaultChainCurrency = json_in.GetBoolField(TEXT("default_chain_currency"));
+		CreatedAt = json_in.GetStringField(TEXT("created_at"));
+		UpdatedAt = json_in.GetStringField(TEXT("updated_at"));
+		DeletedAt = json_in.GetStringField(TEXT("deleted_at"));
 	};
 };
