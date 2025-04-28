85fc707cc45ab5a2cffd55c21f8a935aa4c08427
caballoninja <tomaskolakovic@gmail.com>
2025-04-28 10:22:33 -0300

    Fixed test contract data

--- Plugins/SequenceTests/Source/SequenceTests/Private/CheckoutTests/BuildTransakLinksTests.cpp
+++ Plugins/SequenceTests/Source/SequenceTests/Private/CheckoutTests/BuildTransakLinksTests.cpp
@@ -4,7 +4,7 @@
 #include "SequencePlugin/Public/Marketplace/SequenceMarketplace.h"
 #include "SequencePlugin/Public/Checkout/SequenceCheckout.h"
 #include "SequencePlugin/Public/Types/ERC1155SaleContract.h"
-
+#include "SequencePlugin/Private/Provider.h"
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FTransakCheckoutLinkPrimarySaleTest, "SequencePlugin.EndToEnd.CheckoutTests.Transak.PrimarySaleERC1155", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
 
@@ -14,32 +14,31 @@ bool FTransakCheckoutLinkPrimarySaleTest::RunTest(const FString& Parameters)
 	Checkout->WalletAddress = FString(TEXT("0xD2eFbb2f18bfE3D265b26D2ACe83400A65335a07"));
 	Checkout->Marketplace = NewObject<USequenceMarketplace>();
 	Checkout->Checkout = NewObject<USequenceCheckout>();
-	Checkout->Network = ENetwork::ArbitrumOne;
+	Checkout->Network = ENetwork::PolygonChain;
 
 	UERC1155SaleContract* SaleContract = NewObject<UERC1155SaleContract>();
 
 	SaleContract->ContractAddress = TEXT("0x624ec4ddfb5073b9dd49712036fd56ce895f366e");
-	SaleContract->PaymentToken = TEXT("0xf3C3351D6Bd0098EEb33ca8f830FAf2a141Ea2E1");
-	SaleContract->MaxTotal = 1;
+	SaleContract->PaymentToken = TEXT("0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359");
+	SaleContract->MaxTotal = 2000;
 	SaleContract->Data = TEXT("0x0000000000000000000000000000000000000000000000000000000000000000");
-
-
+	
 	FTransakNFTData NFTData;
-	NFTData.ImageURL = TEXT("https://metadata.sequence.app/projects/158/collections/1233/tokens/0/image.png");
-	NFTData.Name = TEXT("Icy Longsword");
+	NFTData.ImageURL = TEXT("https://dev-metadata.sequence.app/projects/1010/collections/394/tokens/1/image.png");
+	NFTData.Name = TEXT("Keaton T-322");
 	NFTData.CollectionAddress = TEXT("0x8e9609597f7376098ddfc441f45cae5f37057222");
 	NFTData.TokenID = { TEXT("1") };
-	NFTData.Price = { 0 };
+	NFTData.Price = { 0.02 };
 	NFTData.Quantity = 1;
 	NFTData.Type = ENFTType::ERC1155;
 
 	FTransakContractId ContractId;
-	ContractId.Id = "66c5a2cf2fb1688e11fcb167";
-	ContractId.ContractAddress = "0xB537a160472183f2150d42EB1c3DD6684A55f74c";
-	ContractId.Chain = UEnum::GetValueAsString(ENetwork::ArbitrumOne);
+	ContractId.Id = "674eb5613d739107bbd18ed2";
+	ContractId.ContractAddress = "0xe65b75eb7c58ffc0bf0e671d64d0e1c6cd0d3e5b";
+	ContractId.Chain = UEnum::GetValueAsString(ENetwork::PolygonChain);
 	ContractId.PriceTokenSymbol = "USDC";
 
-	TArray<uint8> data;   // Empty Data
+	TArray<uint8> data;
 	TArray<FString> proof;
 	
 	FString Link = Checkout->BuildNFTCheckoutLinkFromERC1155(SaleContract, NFTData, ContractId, data, proof);
